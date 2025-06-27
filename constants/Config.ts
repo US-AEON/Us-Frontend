42b1fb99ab8be56b9aaae86c6025801dccc7f555
LeeWxx <dnckd0903@naver.com>
2025-06-28 03:39:35 +0900

    chore: Add environment variables

---
+++ constants/Config.ts
@@ -0,0 +1,12 @@
+import { API_URL, KAKAO_NATIVE_APP_KEY } from '@env';
+
+// 환경별 설정 관리
+export const Config = {
+  // 서버 API URL
+  API_URL: API_URL,
+  
+  // 카카오 네이티브 앱 키
+  KAKAO_NATIVE_APP_KEY: KAKAO_NATIVE_APP_KEY,
+};
+
+export default Config; 
