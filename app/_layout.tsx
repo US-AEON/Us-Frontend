4c46d47c285da18f775d5d5ff7a3bdda1b05241e
LeeWxx <dnckd0903@naver.com>
2025-06-29 03:12:57 +0900

    chore: Apply Pretendard font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,15 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     ...FontAwesome.font,
+    'Pretendard-Thin': require('../assets/fonts/Pretendard-Thin.ttf'),
+    'Pretendard-ExtraLight': require('../assets/fonts/Pretendard-ExtraLight.ttf'),
+    'Pretendard-Light': require('../assets/fonts/Pretendard-Light.ttf'),
+    'Pretendard-Regular': require('../assets/fonts/Pretendard-Regular.ttf'),
+    'Pretendard-Medium': require('../assets/fonts/Pretendard-Medium.ttf'),
+    'Pretendard-SemiBold': require('../assets/fonts/Pretendard-SemiBold.ttf'),
+    'Pretendard-Bold': require('../assets/fonts/Pretendard-Bold.ttf'),
+    'Pretendard-ExtraBold': require('../assets/fonts/Pretendard-ExtraBold.ttf'),
+    'Pretendard-Black': require('../assets/fonts/Pretendard-Black.ttf'),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
